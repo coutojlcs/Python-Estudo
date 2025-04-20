e0de0ce9e79849edef1670ec4e169d68248b7589
coutojlcs <coutojlcs@hotmail.com>
2025-04-20 12:42:46 -0300

    Primeira estruturad e codigos

--- Primeiro_Programa_Python.py
+++ Primeiro_Programa_Python.py
@@ -1,1 +1,12 @@
+#Declaração de variaveis
+entrada = 100 
+saida = 10
+mensagem1 = "O total calculado é de "
+
+
+total = entrada - saida
+
+
+
 print("hello Word")
+print(f"Alerta {mensagem1}{total}") #format para nãoi declarar cada tipo de variavel
